12cee8c9d727849577b4e0dc5bfb5c281db51968
t0n3d0g <utexas@excavata.com>
2018-12-07 03:59:26 -0600

    first update some functionality

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Allen Freed Burger');
+INSERT INTO burgers (burger_name) VALUES ("Hut's Favorite");
+INSERT INTO burgers (burger_name) VALUES ("Ritchie Valen's Burger");
